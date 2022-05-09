8503a88c84ca8c09d8a4d15ac1a54cdda07fd064
Binru Li <59542200+binruli@users.noreply.github.com>
2022-05-08 23:45:55 -0500

    Expansion panel, repeat helped tool

--- mean-course/src/app/posts/Post-list/post-list.component.ts
+++ mean-course/src/app/posts/Post-list/post-list.component.ts
@@ -7,10 +7,13 @@ import { Component } from "@angular/core";
 })
 
 export class PostListComponent {
-  posts = [
-    {title:'First Post', content: "This is the first post\'s content"},
-    {title:'First Post', content: "This is the second post\'s content"},
-    {title:'First Post', content: "This is the third post\'s content"},
+  //posts = [
+  //  {title:'First Post', content: "This is the first post\'s content"},
+  //  {title:'Second Post', content: "This is the second post\'s content"},
+  //  {title:'Third Post', content: "This is the third post\'s content"},
 
-  ]
+  //];
+  posts = [];
 }
+//stuctural directive: *ngFor helped tool for repeat
+//*ngIf
